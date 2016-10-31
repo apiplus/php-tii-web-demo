82aceecac2234034b5410c4abe7b2bdb3732040d
alacner <alacner@gmail.com>
2016-10-31 16:04:20 +0800

    no message

---
+++ application/page/controllers/index.php
@@ -0,0 +1,10 @@
+<?php
+
+class Page_IndexController extends Tii_Application_Controller_Abstract
+{
+	public function indexAction()
+	{
+		$this->assign('title', Demo_Service::api()->configure_get(["name" => "infoName"]));
+		//$this->assign('navigations', Demo_Service::api()->wanpu_navigation_getTree(["type" => "main"]));
+	}
+}
---
+++ configs/demo.config.php
@@ -0,0 +1,125 @@
+<?php
+
+return [
+	'apidao' => [
+		'appkey' => '92SfjpsHW970fxT5',
+		'secret_key' => 'pwl3HVum0eJ0GToW63sObEOKihRHcdsl',
+		'gateway' => 'http://118.178.167.238',
+	],
+
+	'tabbar' => [
+		[
+			'id' => 'home',
+			'name' => '后端云',
+			'icon' => '/static/images/tabbar/b.png',
+		],
+		[
+			'id' => 'case',
+			'name' => '经典案例',
+			'icon' => '/static/images/tabbar/ca.png',
+		],
+		[
+			'id' => 'cooperation',
+			'name' => '商务合作',
+			'icon' => '/static/images/tabbar/co.png',
+		],
+		[
+			'id' => 'app',
+			'name' => '小应用',
+			'icon' => '/static/images/tabbar/a.png',
+		],
+	],
+
+	'apps' => [
+		[
+			'id' => 'home',
+			'name' => '等风来',
+			'icon' => '/static/images/tabbar/b.png',
+			'url' => '/app/detail.html',
+		],
+		[
+			'id' => 'case',
+			'name' => '魅力江南',
+			'icon' => '/static/images/tabbar/ca.png',
+			'url' => '/app/detail.html',
+		],
+		[
+			'id' => 'cooperation',
+			'name' => '海云商',
+			'icon' => '/static/images/tabbar/co.png',
+			'url' => '/app/detail.html',
+		],
+		[
+			'id' => 'app',
+			'name' => '伊人直选',
+			'icon' => '/static/images/tabbar/a.png',
+			'url' => '/app/detail.html',
+		],
+		[
+			'id' => 'home',
+			'name' => '存梦风',
+			'icon' => '/static/images/tabbar/b.png',
+			'url' => '/app/detail.html',
+		],
+		[
+			'id' => 'case',
+			'name' => '魅力江南',
+			'icon' => '/static/images/tabbar/ca.png',
+			'url' => '/app/detail.html',
+		],
+		[
+			'id' => 'cooperation',
+			'name' => '阿迪文摘',
+			'icon' => '/static/images/tabbar/co.png',
+			'url' => '/app/detail.html',
+		],
+		[
+			'id' => 'app',
+			'name' => '虚席以待',
+			'icon' => '/static/images/tabbar/a.png',
+			'url' => '/app/detail.html',
+		],
+		[
+			'id' => 'cooperation',
+			'name' => '海云商',
+			'icon' => '/static/images/tabbar/co.png',
+			'url' => '/app/detail.html',
+		],
+		[
+			'id' => 'app',
+			'name' => '伊人直选',
+			'icon' => '/static/images/tabbar/a.png',
+			'url' => '/app/detail.html',
+		],
+		[
+			'id' => 'home',
+			'name' => '存梦风',
+			'icon' => '/static/images/tabbar/b.png',
+			'url' => '/app/detail.html',
+		],
+		[
+			'id' => 'case',
+			'name' => '魅力江南',
+			'icon' => '/static/images/tabbar/ca.png',
+			'url' => '/app/detail.html',
+		],
+		[
+			'id' => 'cooperation',
+			'name' => '阿迪文摘',
+			'icon' => '/static/images/tabbar/co.png',
+			'url' => '/app/detail.html',
+		],
+		[
+			'id' => 'app',
+			'name' => '虚席以待',
+			'icon' => '/static/images/tabbar/a.png',
+			'url' => '/app/detail.html',
+		],
+		[
+			'id' => 'app',
+			'name' => '更多',
+			'icon' => '/static/images/tabbar/b.png',
+			'url' => '/app.html',
+		],
+	]
+];
